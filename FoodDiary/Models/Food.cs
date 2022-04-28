46d7596fcb65e8652e58d3121d153382707eed4c
Nathan Center <nathanjcenter@gmail.com>
2022-04-28 11:54:34 -0500

    added list to the foods

---
+++ FoodDiary/Models/Food.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodDiary.Models
+{
+    public class Food
+    { public int Id { get; set; }
+
+        public string FoodName { get; set; }
+
+        public string Description { get; set; }
+
+        public int Caloric { get; set; }
+
+        public FoodSchedule FoodSchedule { get; set; }
+
+    }
+}
---
+++ FoodDiary/Models/FoodSchedule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FoodDiary.Models
+{
+    public class FoodSchedule
+    {
+        public int Id { get; set; }
+        public int UserProfileId { get; set; }
+
+        public int FoodId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Meal { get; set; }
+    }
+}
